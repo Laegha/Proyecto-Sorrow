3a56643117268de01421a61421d24437c30c996e
Laegha <franciscolarghero@gmail.com>
2023-08-19 01:41:02 -0300

    feat(Chase): Comportamiento del monstruo
    Añadido el cálculo de dirección y la detección de waypoint con vectores

--- Sorrow/Assets/Scripts/ChaseController.cs
+++ Sorrow/Assets/Scripts/ChaseController.cs
@@ -8,17 +8,36 @@
 
 //    int trackedWayPoint = 0;
 
+//    [HideInInspector] public bool isMoving;
+
 //    [SerializeField] float speed;
 
-//    void Chase()
-//    {
-//        Vector2 direction = new Vector2 (waypoints[trackedWayPoint].position.x, waypoints[trackedWayPoint].position.z) - new Vector2 (transform.position.x, transform.position.z);
-//        transform.Translate(new Vector3(direction.x, 0, direction.y) * speed);
-//        if(ReachedWaypoint())
-//    }
-//    bool ReachedWaypoint()
-//    {
+//    Vector2 delta;
+//    float direction;
 
-//        if()
+//    private void Start() => TrackWaypoint();
+
+//    private void Update()
+//    {
+//        if (!isMoving)
+//            return;
+
+//        transform.Translate(new Vector3(delta.x, 0, delta.y) * speed * Time.deltaTime);
+//        if (delta.)
+//        {
+//            trackedWayPoint++;
+//            if (trackedWayPoint > waypoints.Length)
+//            {
+//                isMoving = false;
+//                return;
+//            }
+//            TrackWaypoint();
+//        }
+//    }
+
+//    void TrackWaypoint()
+//    {
+//        delta = new Vector2(waypoints[trackedWayPoint].position.x, waypoints[trackedWayPoint].position.z) - new Vector2(transform.position.x, transform.position.z);
+//        direction = Mathf.
 //    }
 //}
