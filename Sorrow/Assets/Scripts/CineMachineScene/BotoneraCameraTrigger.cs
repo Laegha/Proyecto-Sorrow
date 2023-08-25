960119e9a0b8bbb5e7e3998b7581528f313c0214
benipasto <47392632@est.ort.edu.ar>
2023-08-25 10:43:24 -0300

    feat(BotoneraCamera): Cambio de cámaras
    Cambio de cámara entre la main y la BotoneraCamera con CineMachine.

---
+++ Sorrow/Assets/Scripts/CineMachineScene/BotoneraCameraTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Cinemachine;
+
+public class StartCinematic : MonoBehaviour
+{
+    public CinemachineVirtualCameraBase botoneraCamera;
+
+    private bool isCinematicPlaying = false;
+    [SerializeField] KeyCode exitButton=KeyCode.Escape;
+    BoxCollider botoneraTriggerCollider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isCinematicPlaying)
+        {
+            isCinematicPlaying = true;
+            botoneraCamera.Priority = 10; // Asegurarte de que la cámara de la cinemática tenga la prioridad más alta
+
+            // Iniciar la cinemática aquí si es necesario
+            // Por ejemplo, podrías hacer que las cámaras empiecen a seguir rutas definidas
+        }
+
+        if (isCinematicPlaying && Input.GetKeyDown(exitButton))
+        {
+            isCinematicPlaying = false;
+            botoneraCamera.Priority = 1;
+            botoneraTriggerCollider.enabled = false;
+                    
+        }
+    }
+}
+
--- Sorrow/Assets/Scripts/Interactables/RithmInteractable.cs
+++ Sorrow/Assets/Scripts/Interactables/RithmInteractable.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class RithmInteractable : Interactable
 {
     public int empanadas;
+    [SerializeField] CinemachineVirtualCameraBase botoneraCamera;    
     public override void Interaction()
     {
-        base.Interaction();
-
-        print("Interacted with " + transform.name);
+        botoneraCamera.Priority = 10;
+        GameObject.FindGameObjectWithTag("Player").SetActive(false);
     }
 }
