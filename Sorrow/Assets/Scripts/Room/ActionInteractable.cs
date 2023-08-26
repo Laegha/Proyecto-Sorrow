0f734e5fe4c60987d203e34a8cdeeea13011020c
Laegha <franciscolarghero@gmail.com>
2023-08-26 17:17:51 -0300

    feat(Room): Basics
    Añadida escena de room, script de action counter y actionInteractable

---
+++ Sorrow/Assets/Scripts/Room/ActionInteractable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionInteractable : Interactable
+{
+    [SerializeField] UnityEvent onInteractEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
