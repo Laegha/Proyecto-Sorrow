23ffd01cee0111fead634647f39c3ff12b9dc064
Laegha <franciscolarghero@gmail.com>
2023-08-30 22:30:12 -0300

    feat(Interactable): Distancia para el highlight
    Cuando el mouse entra y sale del objeto, se checkea la distancia con el jugador antes de cambiar de color. Esto está mal, ya que si el jugador se aleja mientras tiene el cursor encima, el interactuable permanece highlighteado

--- Sorrow/Assets/Scripts/Interactables/Interactable.cs
+++ Sorrow/Assets/Scripts/Interactables/Interactable.cs
@@ -5,19 +5,32 @@ public class Interactable : MonoBehaviour
 {
     Material interactionMaterial;
 
-    protected virtual void Start() => interactionMaterial = GetComponent<MeshRenderer>().material; /*interactionMaterial = GetComponent<MeshRenderer>().materials.First(m => m.name is "InteractableMaterial (Instance)");*/
+    PlayerInteraction playerInteraction;
+    protected virtual void Start()
+    {
+        playerInteraction = GameObject.FindWithTag("Player").GetComponent<PlayerInteraction>();
+        interactionMaterial = GetComponent<MeshRenderer>().material; /*interactionMaterial = GetComponent<MeshRenderer>().materials.First(m => m.name is "InteractableMaterial (Instance)");*/
+    }
 
     public virtual void Interaction() {}
 
     private void OnMouseEnter()
     {
-        if (enabled)
+        if (!enabled)
+            return;
+
+        float distance = (playerInteraction.transform.position - transform.position).magnitude;
+        if(distance < playerInteraction.interactionDistance)
             interactionMaterial.SetColor("currentColor", Color.white);
     }
 
     private void OnMouseExit()
     {
-        if (enabled)
+        if (!enabled)
+            return;
+
+        float distance = (playerInteraction.transform.position - transform.position).magnitude;
+        if (distance < playerInteraction.interactionDistance)
             interactionMaterial.SetColor("currentColor", Color.black);
     }
 }
