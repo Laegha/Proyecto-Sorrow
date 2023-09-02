475b1f1c5da9c095fca2ae7bb90eae2587c0808b
Laegha <franciscolarghero@gmail.com>
2023-09-02 17:05:43 -0300

    feat(RythmInteractables): Funcionamiento del headphone
    Añadido el script HeadphoneController, que almacena todos los RithmInteractable y cuando te pones los headphones para cada uno llama a una función que cambia la textura y permite interactuar con el

---
+++ Sorrow/Assets/Scripts/Building/HeadphoneController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadphoneController : MonoBehaviour
+{
+    public static List<RithmInteractable> rithmInteractables = new List<RithmInteractable>();
+    [SerializeField] Image headphonesFilter;
+    bool headphonesOn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            SwitchState();
+    }
+    void SwitchState()
+    {
+        headphonesOn = !headphonesOn;
+        headphonesFilter.gameObject.SetActive(headphonesOn);
+        foreach (RithmInteractable rithmInteractable in rithmInteractables)
+            rithmInteractable.SwitchCurrState(headphonesOn);
+    }
+}
--- Sorrow/Assets/Scripts/Interactables/RithmInteractable.cs
+++ Sorrow/Assets/Scripts/Interactables/RithmInteractable.cs
@@ -2,14 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using System.Linq;
 
 public class RithmInteractable : Interactable
 {
-    public int empanadas;
-    [SerializeField] CinemachineVirtualCameraBase botoneraCamera;    
+    //public int empanadas;//esto es un meme
+    [SerializeField] CinemachineVirtualCamera interactableCamera;
+
+    [SerializeField] Texture3D headphonesOnTexture;
+    [SerializeField] Texture3D headphonesOffTexture;
+
+    [SerializeField] string headphonesOnTextureName;
+
+    Material headphonesOnMaterial;
+    protected override void Start()
+    {
+        base.Start();
+
+        headphonesOnMaterial = GetComponent<MeshRenderer>().materials.First(m => m.name == headphonesOnTextureName + " (Instance)");
+        HeadphoneController.rithmInteractables.Add(this);
+    }
+
     public override void Interaction()
     {
-        botoneraCamera.Priority = 10;
+        base.Interaction();
+
+        CinematicManager.CameraChange(interactableCamera);
         GameObject.FindGameObjectWithTag("Player").SetActive(false);
+    }
+
+    public void SwitchCurrState(bool canBeInteracted)
+    {
+        enabled = canBeInteracted;
+        headphonesOnMaterial.SetTexture("_MainTex", canBeInteracted ? headphonesOnTexture : headphonesOffTexture);
+        interactionMaterial.SetFloat("_CanBeInteracted", canBeInteracted ? 1 : 0);
     }
 }
