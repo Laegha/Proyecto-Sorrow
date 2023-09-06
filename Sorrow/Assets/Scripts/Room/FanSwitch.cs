2d6ca050a2775e42f6a69e7385359f880093eda0
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2023-09-06 13:34:57 -0300

    feat(FanSwitch): EndResult

--- Sorrow/Assets/Scripts/Room/FanSwitch.cs
+++ Sorrow/Assets/Scripts/Room/FanSwitch.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class FanSwitch : HitableTarget
 {
+    Transform playerTransform;
+    Rigidbody rb;
+    BoxCollider boxCollider;
+
+    void Awake()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+
+        playerTransform = player.transform;
+        rb = player.GetComponent<Rigidbody>();
+        boxCollider = player.GetComponent<BoxCollider>();
+
+
+        rb.isKinematic = true;
+        boxCollider.enabled = false;
+
+        InputManager.controller.Player.Disable();
+    }
+
     public override void Activate()
     {
         //Iniciar timeline
+        
+    }
+
+    void EndResult() // DEBUG
+    {
+        transform.Translate(Vector3.up);
+        rb.isKinematic = false;
+        boxCollider.enabled = true;
     }
 }
