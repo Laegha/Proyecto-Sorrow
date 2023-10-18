42473906bf823f95dea0500c22ad865c9fbb7d70
Laegha <110439827+Laegha@users.noreply.github.com>
2023-10-18 15:46:18 -0300

    refactor(SandMovement): Shader a codigo
    El offset se calcula por código por un timer que va de 0 a 1 en vez de Time.time. Esto me permite tener mayor control sobre el offset y evitar saltos, pero todavia no logro el efecto

--- Sorrow/Assets/Scripts/DessertScene/DessertTimelineResources.cs
+++ Sorrow/Assets/Scripts/DessertScene/DessertTimelineResources.cs
@@ -7,6 +7,7 @@ public class DessertTimelineResources : MonoBehaviour
     [SerializeField] float movingSandMaxSpeed;
     [SerializeField] Renderer sandRenderer;
     Material sandMaterial => sandRenderer.material;
+    [SerializeField] Vector2 sandMoveDirection;
 
     [SerializeField] BGMovingObject[] bgObjects;
 
@@ -17,35 +18,56 @@ public class DessertTimelineResources : MonoBehaviour
             bgObject.isMoving = isStarting;
     }
 
-    public IEnumerator ChangeMovingSandSpeed(bool isStarting)
-    {
-        float x = 0;
-        float oao = isStarting ? 0 : movingSandMaxSpeed;
-        float y = isStarting ? 0 : movingSandMaxSpeed;
-        float a = movingSandMaxSpeed/1;
-        if (!isStarting)
-            a *= -1;
-
-        while(isStarting ? y < movingSandMaxSpeed : y > 0)
-        {
-            yield return new WaitForEndOfFrame();
-            
-            x += Time.deltaTime;
-            y = a * (x * x) + oao;
-            sandMaterial.SetFloat("_Speed", y);
-            print("y: " + y + " Speed: " + sandMaterial.GetFloat("_Speed"));
-        }
-        //sandMaterial.SetFloat("_Speed", isStarting ? movingSandMaxSpeed : 0);
-    }
-
     bool sandMoving = false;
+    Vector2 currOffset;
+    float speed;
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.M))
         {
-            sandMoving = !sandMoving;
-            ChangeWalkMode(sandMoving);
+            ChangeWalkMode(!sandMoving);
+        }//PLACEHOLDER
+
+        currOffset = sandMoveDirection * speed * time;
+        sandMaterial.SetVector("_TextureOffset", currOffset);
+        print(currOffset);
+    }
+
+    float time = 1;
+
+    IEnumerator Timer()
+    {
+        while(true)
+        {
+            time += Time.deltaTime;
+            if (time > 2)
+                time = 1;
         }
     }
+
+    public IEnumerator ChangeMovingSandSpeed(bool isStarting)
+    {
+        sandMaterial.SetFloat("_AutoMove", 0);
+        float x = 0;
+        float oao = isStarting ? 0 : movingSandMaxSpeed;
+        float a = movingSandMaxSpeed/2;
+        if (!isStarting)
+            a *= -1;
+        while (isStarting ? speed < movingSandMaxSpeed : speed > 0)
+        {
+            yield return new WaitForEndOfFrame();
+
+            //sandMaterial.SetFloat("_Speed", y);
+            x += Time.deltaTime;
+            speed = a * (x * x) + oao;
+        }
+        speed = isStarting ? movingSandMaxSpeed : 0;
+        yield return new WaitForEndOfFrame();
+        sandMoving = isStarting;
+        //sandMaterial.SetFloat("_Speed", isStarting ? movingSandMaxSpeed : 0);
+        //sandMaterial.SetFloat("_AutoMove", 1);
+    }
+
+
 }
