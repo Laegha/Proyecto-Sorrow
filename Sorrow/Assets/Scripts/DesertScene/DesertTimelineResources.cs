6e0618b88e19d6fdd2a19e7130249417f7c806cd
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2023-10-24 09:46:33 -0300

    fix(DesertTimelineResources): rename class

--- Sorrow/Assets/Scripts/DesertScene/DesertTimelineResources.cs
+++ Sorrow/Assets/Scripts/DesertScene/DesertTimelineResources.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DessertTimelineResources : MonoBehaviour
+public class DesertTimelineResources : MonoBehaviour
 {
     [SerializeField] float movingSandMaxSpeed;
     [SerializeField] Renderer sandRenderer;
