896058c9bde0ffefa0c0306caf3af40e96a6e4af
Laegha <110439827+Laegha@users.noreply.github.com>
2023-10-25 15:21:03 -0300

    fix(ElevatorTimeline): Signal en vez de animation clip
    En vez de un animation track uso un signal track para triggerear la animacion desde el animator y que asi quede abierta

--- Sorrow/Assets/Scripts/ElevatorScene/ElevatorArrival.cs
+++ Sorrow/Assets/Scripts/ElevatorScene/ElevatorArrival.cs
@@ -13,6 +13,7 @@ public class ElevatorArrival : MonoBehaviour
     {
         GameObject.FindWithTag("Player").transform.SetParent(elevator);
         elevator.position = newElevatorPosition;
+        GameObject.FindWithTag("Player").transform.SetParent(null);
 
         endTimeline.Play();
     }
