ec246d4e677b0247e6cba9445e6ea118008412b9
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-10-25 19:51:24 -0300

    fix(PortraitInspector): transforms
    Good bye reflections, it was fun while it lasted.

--- Sorrow/Assets/Scripts/RoomScene/PortraitInspector.cs
+++ Sorrow/Assets/Scripts/RoomScene/PortraitInspector.cs
@@ -10,38 +10,46 @@ public class PortraitInspector : Interactable
     [SerializeField] Transform lookingTransform;
     [SerializeField] float transitionSpeed = 1f;
     [SerializeField] Vector2 rotationCaps;
-    Transform originalTransform;
+    Vector3 originalPosition;
+    Quaternion originalRotation;
     bool isLooking = false;
     float transitionTime = 1f;
     Vector2 currRot, maxRot, minRot;
+    CameraLook cameraLook;
 
     protected override void Awake()
     {
         base.Awake();
-        originalTransform = typeof(object).GetMethod("MemberwiseClone", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(transform, Array.Empty<object>()) as Transform;
+        //originalTransform = typeof(object).GetMethod("MemberwiseClone", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(transform, Array.Empty<object>()) as Transform;
+        originalPosition = transform.position; // Sometimes dark magic is fun
+        originalRotation = transform.rotation; // But sometimes being greedy is better
         minRot = maxRot = lookingTransform.eulerAngles;
         minRot -= rotationCaps;
         maxRot += rotationCaps;
+        cameraLook = FindObjectOfType<CameraLook>();
     }
 
     public override void Interaction()
     {
         isLooking ^= true;
-        transitionTime = 0f;
+        transitionTime = isLooking ? 0f : 1f;
+        cameraLook.enabled = !isLooking;
+        if (isLooking)
+            InputManager.controller.Camera.Enable();
+        else
+            lookingTransform.rotation = transform.rotation;
         currRot = lookingTransform.eulerAngles;
-        if (!isLooking)
-            CinematicManager.instance.playerCamera.transform.rotation = Quaternion.identity;
         CinematicManager.instance.CameraChange(isLooking ? portraitCamera : CinematicManager.instance.playerCamera);
     }
 
     void Update()
     {
-        if (transitionTime < 1f)
+        if (0f <= transitionTime && transitionTime <= 1f)
         {
-            transitionTime += Time.deltaTime * transitionSpeed;
+            transitionTime += Time.deltaTime * transitionSpeed * (isLooking ? 1f : -1f);
             transform.SetPositionAndRotation(
-                Vector3.Lerp(originalTransform.position, lookingTransform.position, transitionTime),
-                Quaternion.Lerp(originalTransform.rotation, lookingTransform.rotation, transitionTime)
+                Vector3.Lerp(originalPosition, lookingTransform.position, transitionTime),
+                Quaternion.Lerp(originalRotation, lookingTransform.rotation, transitionTime)
             );
             return;
         } else if (!isLooking) return;
