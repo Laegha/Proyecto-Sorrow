661c9ec0847471ba997d8dfa479f63bdbd2ab5c1
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-11-07 19:01:51 -0300

    feat(FanSwitch): UnityEvent + SwitchModification

--- Sorrow/Assets/Scripts/RoomScene/FanSwitch.cs
+++ Sorrow/Assets/Scripts/RoomScene/FanSwitch.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Playables;
 
 public class FanSwitch : HitableTarget
 {
+    [SerializeField] GameObject button;
+    [SerializeField] UnityEvent endActions;
+    MeshRenderer buttonMR;
+    PlayableDirector playableDirector;
+
+    void Awake()
+    {
+        buttonMR = button.GetComponent<MeshRenderer>();
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+
     public override void Activate()
     {
-        GetComponent<PlayableDirector>().Play();
-        enabled = false;
+        button.transform.Rotate(0f, -button.transform.rotation.eulerAngles.y, 0f);
+        buttonMR.material.DisableKeyword("_EMISSION");
+        StartCoroutine(PlayTimeline());
+    }
+
+    IEnumerator PlayTimeline()
+    {
+        playableDirector.Play();
+        yield return new WaitForSeconds((float)playableDirector.duration);
+        endActions.Invoke();
         Destroy(this);
     }
 }
