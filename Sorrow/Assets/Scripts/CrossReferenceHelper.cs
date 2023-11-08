37bf3bc9c7c124dc7d4647451d217387f0a7c9ee
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2023-11-08 15:59:57 -0300

    feat(CrossReferenceHelper): Functions & Fields

--- Sorrow/Assets/Scripts/CrossReferenceHelper.cs
+++ Sorrow/Assets/Scripts/CrossReferenceHelper.cs
@@ -6,29 +6,65 @@ using UnityEngine;
 
 public class CrossReferenceHelper : MonoBehaviour
 {
-    [Serializable]
-    public struct ReferenceArguments
+    public class ReferenceArguments
     {
         public string objectName;
         public string componentName;
-        public string functionName;
+        public string componentSubclass;
+        public string lookingFor;
+    }
+
+    [Serializable]
+    public class FunctionReferenceArguments : ReferenceArguments
+    {
         public string[] arguments;
         public string[] argumentTypes;
         public object[] convertedArguments;
     }
 
-    [SerializeField] ReferenceArguments[] referenceArguments;
+    [Serializable]
+    public class FieldReferenceArguments : ReferenceArguments
+    {
+        public string fieldType;
+        public string newValue;
+        public object convertedValue;
+    }
+
+    [SerializeField] FunctionReferenceArguments[] functionRA;
+    [SerializeField] FieldReferenceArguments[] fieldRA;
 
     void Awake()
     {
-        foreach (var r in referenceArguments)
+        foreach (var r in functionRA)
             foreach (var (a, t) in r.arguments.Zip(r.argumentTypes, (a, t) => (a, t)))
                 r.convertedArguments.Append(Convert.ChangeType(a, Type.GetType(t)));
+
+        foreach (var r in fieldRA)
+            r.convertedValue = Convert.ChangeType(r.newValue, Type.GetType(r.fieldType));
     }
 
-    public void Call(int index)
+    public void CallFunction(int index)
     {
-        var component = GameObject.Find(referenceArguments[index].objectName).GetComponent(referenceArguments[index].componentName);
-        component.GetType().GetMethod(referenceArguments[index].functionName).Invoke(component, referenceArguments[index].convertedArguments);
+        var component = ComponentGetter(functionRA[index]);
+        var type = TypeGetter(component, fieldRA[index]);
+        type.GetMethod(functionRA[index].lookingFor).Invoke(component, functionRA[index].convertedArguments);
+    }
+
+    public void ModifyFieldValue(int index)
+    {
+        var component = ComponentGetter(fieldRA[index]);
+        var type = TypeGetter(component, fieldRA[index]);
+        type.GetField(fieldRA[index].lookingFor).SetValue(component, fieldRA[index].convertedValue);
+    }
+
+    Component ComponentGetter(ReferenceArguments ra)
+        => GameObject.Find(ra.objectName).GetComponent(ra.componentName);
+
+    Type TypeGetter(Component component, ReferenceArguments ra)
+    {
+        if (ra.componentSubclass != string.Empty && ra.componentSubclass != null)
+            return Type.GetType(ra.componentSubclass);
+        
+        return component.GetType();
     }
 }
