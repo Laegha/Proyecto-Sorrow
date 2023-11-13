fba304721b323d35829988c8f73f4f5a711f4013
Laegha <franciscolarghero@gmail.com>
2023-11-12 21:20:24 -0300

    fix(Billboard): El bullseye se escala con el tamaño del parent
    Funciona solo cuando corres el juego, en el editor no se va a ver

--- Sorrow/Assets/Scripts/StreetScene/Billboard.cs
+++ Sorrow/Assets/Scripts/StreetScene/Billboard.cs
@@ -6,7 +6,11 @@ public class Billboard : MonoBehaviour
 {
     Camera mainCamera;
 
-    void Start() => mainCamera = Camera.main;
+    void Start()
+    {
+        transform.localScale = transform.parent.localScale / 0.3f;
+        mainCamera = Camera.main;
+    }
 
     void Update()
     {
