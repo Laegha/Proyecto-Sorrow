f5a4e9389465d13ec962289f1248b03cefa25197
BlurOne-GIT <tommyspurio@gmail.com>
2023-11-14 09:02:38 -0300

    feat(Billboard): StandardScale

--- Sorrow/Assets/Scripts/StreetScene/Billboard.cs
+++ Sorrow/Assets/Scripts/StreetScene/Billboard.cs
@@ -6,10 +6,13 @@ public class Billboard : MonoBehaviour
 {
     Camera mainCamera;
 
+    static readonly Vector3 standardScale = new(.4f, .4f, .4f);
+
     void Start()
     { 
         mainCamera = Camera.main;
-        transform.SetParent(null);     
+        transform.SetParent(null);
+        transform.localScale = standardScale;
     }
     void Update()
     {
