347b53ad0895ce72bc7b5e55f4b9cb0970e5f63c
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-04 02:39:09 -0300

    feat(MainMenu): Fade to black

--- Sorrow/Assets/Scripts/UI/PantallaInicio.cs
+++ Sorrow/Assets/Scripts/UI/PantallaInicio.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PantallaInicio : MonoBehaviour
 {
     //GameObject opciones;
     [SerializeField] GameObject pantallaOpciones;
+    [SerializeField] GameObject blackPanel;
+    [SerializeField] float fadeSpeed = 1f;
+    Image blackScreen;
+    bool starting = false;
+    float timer = 0f;
+
+    void Awake() => blackScreen = blackPanel.GetComponent<Image>();
+
     //void Awake()
     //{
     //    opciones = GameObject.Fin("PantallaOpciones"); (no funca, sad)
@@ -14,7 +23,25 @@ public class PantallaInicio : MonoBehaviour
     public void StartGame()
     {
         print("Load StreetScene");
-        SceneManager.LoadScene("StreetScene");
+        blackPanel.SetActive(true);
+        starting = true;
+    }
+
+    void LoadStreetScene() => SceneManager.LoadScene("StreetScene");
+
+    void Update()
+    {
+        if (!starting)
+            return;
+
+        blackScreen.color = new(0f, 0f, 0f, Mathf.Clamp01(timer));
+        timer += Time.deltaTime * fadeSpeed;
+
+        if (timer <= 1f)
+            return;
+        
+        starting = false;
+        LoadStreetScene();
     }
 
     public void OptionsButton()
