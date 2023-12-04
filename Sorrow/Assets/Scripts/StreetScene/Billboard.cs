3210ce7c36e1c95cc91ecd306edd47258c6458c0
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-03 22:53:51 -0300

    perf(Billboard): Unnecessary Tracking

--- Sorrow/Assets/Scripts/StreetScene/Billboard.cs
+++ Sorrow/Assets/Scripts/StreetScene/Billboard.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,18 @@ public class Billboard : MonoBehaviour
     Camera mainCamera;
 
     static readonly Vector3 standardScale = new(.4f, .4f, .4f);
+    static GameObject bullseyeParent;
 
     void Start()
     { 
         mainCamera = Camera.main;
-        transform.SetParent(null);
+        if (!bullseyeParent)
+        {
+            bullseyeParent = GameObject.Find("BullseyeParent");
+            bullseyeParent.SetActive(false);
+        }
+
+        transform.SetParent(bullseyeParent.transform);
         transform.localScale = standardScale;
     }
     void Update()
