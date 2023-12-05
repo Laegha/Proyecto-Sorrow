3ed7e8d15dc35e3d60c028205876b9964d0f5ae7
BlurOne-GIT <tommyspurio@gmail.com>
2023-12-05 10:27:50 -0300

    feat(ProximityShaker): Vignette
    Co-Authored-By: beni <110981091+benipasto@users.noreply.github.com>

--- Sorrow/Assets/Scripts/StreetScene/ProximityShaker.cs
+++ Sorrow/Assets/Scripts/StreetScene/ProximityShaker.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class ProximityShaker : MonoBehaviour
 {
     [SerializeField] Transform distanceTo;
     [SerializeField] float minDistance = 60f;
     [SerializeField] float maxShake = 2f;
-    float distanceMultiplier;
-    Vector3 lastSelfPos;
-    Vector3 lastToPos;
+    [SerializeField] Volume volume;
+    [SerializeField] float maxVignette = .6f;
+    float shakeMultiplier, vignetteMultiplier;
+    Vector3 lastSelfPos, lastToPos;
+    float minVignette;
+    Vignette vignette;
 
-    void Awake() => RecalculateDistanceMultiplier();
+    void Awake()
+    {
+        _ = volume.profile.TryGet<Vignette>(out vignette);
+        minVignette = vignette.intensity.value;
+        RecalculateDistanceMultiplier();
+    }
 
-    void OnDisable() => CinematicManager.instance.StopCameraShake();
+    void OnDisable()
+    {
+        CinematicManager.instance.StopCameraShake();
+        //vignette.intensity.value = minVignette;
+    }
 
     void Update()
     {
@@ -27,9 +41,14 @@ public class ProximityShaker : MonoBehaviour
         var distance = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(distanceTo.position.x, distanceTo.position.z));
 
         if (distance > minDistance)
+        {
             CinematicManager.instance.StopCameraShake();
-        else
-            CinematicManager.instance.StartCameraShake(distanceMultiplier * Mathf.Pow(minDistance - distance, 2));
+            vignette.intensity.value = minVignette;
+            return;
+        }
+
+        CinematicManager.instance.StartCameraShake(shakeMultiplier * Mathf.Pow(minDistance - distance, 2));
+        vignette.intensity.value = minVignette + vignetteMultiplier * Mathf.Pow(minDistance - distance, 2);
     }
 
     public void ChangeMinDistance(float newMinDistance)
@@ -44,5 +63,9 @@ public class ProximityShaker : MonoBehaviour
         RecalculateDistanceMultiplier();
     }
 
-    void RecalculateDistanceMultiplier() => distanceMultiplier = maxShake / Mathf.Pow(minDistance - 22.5f, 2);
+    void RecalculateDistanceMultiplier()
+    {
+        shakeMultiplier = maxShake / Mathf.Pow(minDistance - 22.5f, 2);
+        vignetteMultiplier = (maxVignette - minVignette) / Mathf.Pow(minDistance - 22.5f, 2);
+    }
 }
