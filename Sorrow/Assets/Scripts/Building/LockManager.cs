8f30c78ad7fac61a66df4539d426f876184ba852
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-11 15:59:24 -0300

    refactor(LockManager): Single Disc

--- Sorrow/Assets/Scripts/Building/LockManager.cs
+++ Sorrow/Assets/Scripts/Building/LockManager.cs
@@ -5,55 +5,43 @@ using UnityEngine;
 
 public class LockManager : MonoBehaviour
 {
-    Transform[] nums;
+    int selfPosition;
 
-    void Awake() => nums = GetComponentsInChildren<Transform>().Where(t => t is not RectTransform && t != transform).ToArray();
+    void Awake() => selfPosition = name[^2] - 48;
 
     void OnEnable()
     {
-        LockRythmController.OnRotate += Rotate;
-        LockRythmController.OnUnlock += UnlockNums;
+        LockRhythmController.OnRotate += Rotate;
+        LockRhythmController.OnUnlock += UnlockNums;
     }
 
     void OnDisable()
     {
-        LockRythmController.OnRotate -= Rotate;
-        LockRythmController.OnUnlock -= UnlockNums;
+        LockRhythmController.OnRotate -= Rotate;
+        LockRhythmController.OnUnlock -= UnlockNums;
     }
 
-    void Rotate(object _, LockRythmController.LockEventArgs e)
+    void Rotate(object _, LockRhythmController.LockEventArgs e)
     {
-        float rotation = e.CurrentBeat switch
-        {
-            1 => 0f,
-            2 => -90f,
-            3 => -180f,
-            4 => -270f,
-            _ => 0f
-        };
-        
-        var euler = new Vector3(0f, 0f, rotation);
-
-        for (int i = e.LockedNums; i < 32; i++)
-            nums[i].eulerAngles = euler;
+        if (e.CurrentBeat >= selfPosition)
+            return;
+            
+        transform.eulerAngles = new(0f, 0f, CalcRotate(e.CurrentBeat));
         // Activate the animation
     }
 
-    void UnlockNums(object _, LockRythmController.LockEventArgs e)
+    void UnlockNums(object _, LockRhythmController.LockEventArgs e)
     {
-        float rotation = e.CurrentBeat switch
-        {
-            1 => 0f,
-            2 => -90f,
-            3 => -180f,
-            4 => -270f,
-            _ => 0f
-        };
-
-        var euler = new Vector3(0f, 0f, rotation);
-
-        for (int i = e.LockedNums; i < 32; i++)
-            nums[i].eulerAngles = euler;
+        transform.eulerAngles = new(0f, 0f, CalcRotate(e.CurrentBeat));
         // Animate
     }
+
+    float CalcRotate(int beat) => beat switch
+    {
+        1 => 0f,
+        2 => -90f,
+        3 => -180f,
+        4 => -270f,
+        _ => 0f
+    };
 }
