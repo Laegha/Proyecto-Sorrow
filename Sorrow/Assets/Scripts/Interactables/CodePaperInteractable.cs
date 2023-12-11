96858f023d063ff8c0edd22bd5e0d3a2e38c0b96
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-11 20:56:59 -0300

    refactor(CodePaperInteractable): Remove Unused Coroutines

--- Sorrow/Assets/Scripts/Interactables/CodePaperInteractable.cs
+++ Sorrow/Assets/Scripts/Interactables/CodePaperInteractable.cs
@@ -7,9 +7,6 @@ using UnityEngine.Playables;
 public class CodePaperInteractable : OnPickupActionInteractable
 {
     [SerializeField] TextMeshPro[] codeTexts;
-    [SerializeField] Transform targetToLook;
-    [SerializeField] float playerRotationSpeed;
-    [SerializeField] float cameraRotationSpeed;
 
     void Start()
     {
@@ -18,45 +15,6 @@ public class CodePaperInteractable : OnPickupActionInteractable
             codeTexts[i].text = "";
             for (int j = 0; j < 8; j++)
                 codeTexts[i].text += LockRhythmController.finalPin.GetValue(i, j).ToString();
-        }
-    }
-
-    public void PlayerRotate() => StartCoroutine(RotatePlayer());
-
-    IEnumerator RotatePlayer()
-    {
-        InputManager.instance.RemRegControl(false);
-        
-        StartCoroutine(RotateCamera());
-
-        Transform player = CinematicManager.instance.player.transform;
-        Vector2 delta = new Vector2(targetToLook.position.x, targetToLook.position.z) - new Vector2(player.position.x, player.position.z);
-
-        float rotationAngle = Mathf.Atan2(delta.x, delta.y) * Mathf.Rad2Deg;
-
-        player.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
-
-        yield return new WaitForEndOfFrame();
-
-        while (player.rotation.eulerAngles.y > rotationAngle)
-        {
-            player.Rotate(0, Time.deltaTime * -playerRotationSpeed, 0);
-            yield return new WaitForEndOfFrame();
-        }
-
-        player.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.Interpolate;
-        InputManager.instance.RemRegControl(true);
-    }
-
-    IEnumerator RotateCamera()
-    {
-        Transform camera = CinematicManager.instance.playerCamera.transform;
-        float counter = camera.localRotation.eulerAngles.x;
-        while (counter > 0.5f)
-        {
-            counter -= Time.deltaTime * cameraRotationSpeed;
-            camera.Rotate(Time.deltaTime * -cameraRotationSpeed, 0, 0);
-            yield return new WaitForEndOfFrame();
         }
     }
 }
