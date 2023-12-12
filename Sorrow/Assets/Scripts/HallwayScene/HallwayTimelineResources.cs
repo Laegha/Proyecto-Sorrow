e00334913bb8b07cb6002c902f6bbd7d4ab02956
BlurOne-GIT <tommyspurio@gmail.com>
2023-12-12 14:26:13 -0300

    feat(HallwayTimelineResources): Text

--- Sorrow/Assets/Scripts/HallwayScene/HallwayTimelineResources.cs
+++ Sorrow/Assets/Scripts/HallwayScene/HallwayTimelineResources.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
@@ -9,21 +10,30 @@ public class HallwayTimelineResources : MonoBehaviour
 {
     [SerializeField] GameObject lights;
     [SerializeField] MeshRenderer[] lightObjects;
+    [SerializeField] MeshRenderer[] textObjects;
     [SerializeField] Volume volume;
     [SerializeField] float offPostExposure;
-    float onPostExposure;
+    [SerializeField] PlayableDirector onOffDirector;
+    [SerializeField] float minRandomSpeed, maxRandomSpeed, offTextMultiplier;
+    float onPostExposure, onTextMultiplier;
     ColorAdjustments colorAdjustments;
     Material[] lightsMaterials;
+    Material[] textMaterials;
     
     Color lightsOnColor;
     [SerializeField] Color lightsOffColor;
 
     private void Awake()
     {
+        onTextMultiplier = 1f / offTextMultiplier;
+
         lightsMaterials = new Material[lightObjects.Length];
         for (int i = 0; i < lightObjects.Length; i++)
             lightsMaterials[i] = lightObjects[i].materials.First(m => m.name is "Lights (Instance)");
         lightsOnColor = lightsMaterials[0].GetColor("_Emission");
+
+        foreach (MeshRenderer textObject in textObjects)
+            textMaterials.Append(textObject.material);
 
         _ = volume.profile.TryGet<ColorAdjustments>(out colorAdjustments);
         onPostExposure = colorAdjustments.postExposure.value;
@@ -35,6 +45,12 @@ public class HallwayTimelineResources : MonoBehaviour
         lights.SetActive(on);
         foreach (Material light in lightsMaterials)
             light.SetColor("_Emission", on ? lightsOnColor : lightsOffColor);
+        
+        foreach (Material text in textMaterials)
+            text.SetColor("_FaceColor", text.GetColor("_FaceColor") * (on ? offTextMultiplier : onTextMultiplier));
+        
+        var playable = onOffDirector.playableGraph.GetRootPlayable(0);
+        PlayableExtensions.SetSpeed(playable, Random.Range(minRandomSpeed, maxRandomSpeed));
     }
 
     public void CloseElevator() => GameObject.Find("ElevatorDoor").GetComponent<Animator>().Play("DoorClose");
