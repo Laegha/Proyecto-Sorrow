4dcd2960e3f5d6a09e6417c91e0ff3042631aa93
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-11 21:27:22 -0300

    feat(HallwayTimelineResources): Switch Post Exposure
    Co-Authored-By: beni <110981091+benipasto@users.noreply.github.com>

--- Sorrow/Assets/Scripts/HallwayScene/HallwayTimelineResources.cs
+++ Sorrow/Assets/Scripts/HallwayScene/HallwayTimelineResources.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class HallwayTimelineResources : MonoBehaviour
 {
     [SerializeField] GameObject lights;
     [SerializeField] MeshRenderer[] lightObjects;
+    [SerializeField] Volume volume;
+    [SerializeField] float offPostExposure;
+    float onPostExposure;
+    ColorAdjustments colorAdjustments;
     Material[] lightsMaterials;
     
     Color lightsOnColor;
@@ -18,10 +24,14 @@ public class HallwayTimelineResources : MonoBehaviour
         for (int i = 0; i < lightObjects.Length; i++)
             lightsMaterials[i] = lightObjects[i].materials.First(m => m.name is "Lights (Instance)");
         lightsOnColor = lightsMaterials[0].GetColor("_Emission");
+
+        _ = volume.profile.TryGet<ColorAdjustments>(out colorAdjustments);
+        onPostExposure = colorAdjustments.postExposure.value;
     }
 
     public void SwitchLights(bool on)
     {
+        colorAdjustments.postExposure.value = on ? onPostExposure : offPostExposure;
         lights.SetActive(on);
         foreach (Material light in lightsMaterials)
             light.SetColor("_Emission", on ? lightsOnColor : lightsOffColor);
