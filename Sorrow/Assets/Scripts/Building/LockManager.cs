58785ec2a5802c1609d9480ee827475bc527284d
BlurOne-GIT <tommyspurio@gmail.com>
2023-12-12 09:22:17 -0300

    fix(LockManager): Rotation

--- Sorrow/Assets/Scripts/Building/LockManager.cs
+++ Sorrow/Assets/Scripts/Building/LockManager.cs
@@ -9,19 +9,24 @@ public class LockManager : MonoBehaviour
     int selfPosition;
     float timer = 1.1f;
     float timeMultiplier, initRotation, finalRotation;
+    float accRotation = 0f;
 
-    void Awake() => selfPosition = name[^2] - 48;
+    void Awake()
+    {
+        selfPosition = name[^2] - 48;
+        accRotation = transform.eulerAngles.z;
+    }
 
     void OnEnable()
     {
         LockRhythmController.OnRotate += SetupRotation;
-        LockRhythmController.OnUnlock += UnlockNums;
+        LockRhythmController.OnUnlock += UnlockNum;
     }
 
     void OnDisable()
     {
         LockRhythmController.OnRotate -= SetupRotation;
-        LockRhythmController.OnUnlock -= UnlockNums;
+        LockRhythmController.OnUnlock -= UnlockNum;
     }
 
     void Update()
@@ -38,23 +43,29 @@ public class LockManager : MonoBehaviour
     void SetupRotation(object _, LockRhythmController.LockEventArgs e)
     {
         
-        if (selfPosition <= e.CurrentBeat)
+        if (selfPosition <= e.LockedNums)
             return;
 
         timer = 0f;
         timeMultiplier = 1f / e.InTime;
         initRotation = transform.eulerAngles.z;
-        finalRotation = LockRhythmController.CalcRotate(e.CurrentBeat);
+        finalRotation = initRotation - 90f; //LockRhythmController.CalcRotate(e.CurrentBeat) + accRotation;
+        //print($"Init: {initRotation}, Final: {finalRotation}");
     }
 
-    void UnlockNums(object _, LockRhythmController.LockEventArgs e)
+    void UnlockNum(object _, LockRhythmController.LockEventArgs e)
     {
-        transform.eulerAngles = new(0f, 0f, LockRhythmController.CalcRotate(e.CurrentBeat));
+        accRotation = 0f;
+        var rotated = transform.eulerAngles;
+        rotated.z = accRotation + LockRhythmController.CalcRotate(e.CurrentBeat);
+        transform.eulerAngles = rotated;
         // Animate
     }
 
     public static float EaseInExpo(float start, float end, float value)
     {
+        value = Mathf.Clamp01(value);
+
         end -= start;
         return end * Mathf.Pow(2, 10 * (value - 1)) + start;
     }
