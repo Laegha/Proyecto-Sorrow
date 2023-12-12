e9afc5120b181386d9e214f973d08c739832d1ad
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-11 21:02:31 -0300

    refactor(CodePaper): Removed Unnecessary Interactable Inheritance

--- Sorrow/Assets/Scripts/HallwayScene/CodePaper.cs
+++ Sorrow/Assets/Scripts/HallwayScene/CodePaper.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class CodePaperInteractable : OnPickupActionInteractable
+public class CodePaper : MonoBehaviour
 {
     [SerializeField] TextMeshPro[] codeTexts;
 
