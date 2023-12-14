2ba9eaddef1d4f1f5fc34d1e023a585c62ff3ae1
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-13 22:34:31 -0300

    refactor(ElevatorArrival): Contained by Elevator Object

--- Sorrow/Assets/Scripts/ElevatorScene/ElevatorArrival.cs
+++ Sorrow/Assets/Scripts/ElevatorScene/ElevatorArrival.cs
@@ -7,12 +7,11 @@ public class ElevatorArrival : MonoBehaviour
 {
     [SerializeField] PlayableDirector endTimeline;
     [SerializeField] Vector3 newElevatorPosition;
-    [SerializeField] Transform elevator;
 
     public void StartArrival()
     {
-        GameObject.FindWithTag("Player").transform.SetParent(elevator);
-        elevator.SetPositionAndRotation(newElevatorPosition, Quaternion.Euler(0, 180, 0));
+        GameObject.FindWithTag("Player").transform.SetParent(transform);
+        transform.SetPositionAndRotation(newElevatorPosition, Quaternion.Euler(0, 180, 0));
         GameObject.FindWithTag("Player").transform.SetParent(null);
 
         endTimeline.Play();
