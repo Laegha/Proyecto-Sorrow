86c7968b6d4d1fc052a7196933233c5f8a23d7a9
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-12-14 04:12:08 -0300

    feat(KeyPadInteractable): Anew
    Split functionality into cutscene and controller. Simplified logic (perf).

--- Sorrow/Assets/Scripts/Interactables/KeyPadInteractable.cs
+++ Sorrow/Assets/Scripts/Interactables/KeyPadInteractable.cs
@@ -4,155 +4,64 @@ using UnityEngine;
 using System.Linq;
 using Cinemachine;
 
-public class KeyPadInteractable : RithmInteractable
+public class KeyPadInteractable : ActionInteractable
 {
-
-    [HideInInspector] public List<KeypadButton> buttons = new List<KeypadButton>();
-
-    [SerializeField] Beat[] beats;
-
-    int interactedTimes = 0;
-
+    static int interactedTimes = 0;
     [SerializeField] GameObject[] depressionPositions;
     [SerializeField] CinemachineVirtualCamera depressionCamera;
-    
-    protected override void Awake()
+    [SerializeField] Texture[] keypadTextures;
+    [SerializeField] Renderer[] renderers;
+    CameraLook cameraLook;
+    KeypadRhythmController kprController;
+
+    void Start()
     {
-        base.Awake();
-        enabled = true;
-        buttons = GetComponentsInChildren<KeypadButton>().ToList();
-        
-        foreach (KeypadButton button in buttons)
-        {
-            button.keyPadInteractable = this;
-            button.gameObject.SetActive(false);
-        }
+        var player = GameObject.Find("Player");
+        cameraLook = player.GetComponentInChildren<CameraLook>();
+        depressionCamera.transform.parent = player.transform;
+        depressionCamera.transform.localPosition = CinematicManager.instance.playerCamera.transform.localPosition;
+        kprController = FindObjectOfType<KeypadRhythmController>();
+        GetComponent<Outline>().enabled = true;
+    }
+
+    void OnDisable()
+    {
+        if (!gameObject.scene.IsValid())
+            return;
+
+        Destroy(depressionCamera.gameObject);
+        kprController.enabled = true;
     }
 
     public override void Interaction()
     {
-        if (interactedTimes > depressionPositions.Length)
-            return;
-
-        if (interactedTimes == depressionPositions.Length - 1)
-        {
-            base.Interaction();
-            interactedTimes++;
-            return;
-
-        }
-
-        depressionPositions[interactedTimes].SetActive(false);
-        interactedTimes++;
-        depressionPositions[interactedTimes].SetActive(true);
-        if(interactedTimes < headphonesOffTextures.Length)
-            headphonesOnMaterial.SetTexture("_MainTex", headphonesOffTextures[interactedTimes]);
-
-        StartCoroutine(LookAtDepression());
-
+        base.Interaction();
         if (interactedTimes == depressionPositions.Length - 1)
         {
             enabled = false;
-            useHeadphones = true;
+            return;
         }
 
+        depressionPositions[interactedTimes++].SetActive(false);
+        depressionPositions[interactedTimes].SetActive(true);
+        depressionCamera.LookAt = depressionPositions[interactedTimes].transform.GetChild(0).GetChild(0).Find("Face"); // Not proud of this either
+
+        LookAtDepression();
     }
-    IEnumerator LookAtDepression()
+
+    void LookAtDepression()
     {
-        Transform player = CinematicManager.instance.player.transform;
-        var delta = new Vector2(depressionPositions[interactedTimes].transform.position.x, depressionPositions[interactedTimes].transform.position.z) - new Vector2(player.position.x, player.position.z);
-        float playerRotation = Mathf.Atan2(delta.x, delta.y) * Mathf.Rad2Deg;
-
-        Transform camera = CinematicManager.instance.playerCamera.transform;
-        var camDelta = new Vector2(depressionPositions[interactedTimes].transform.position.x, depressionPositions[interactedTimes].transform.position.y) - new Vector2(camera.position.x, camera.position.y);
-        float camRotation = Mathf.Atan2(camDelta.y, camDelta.x);
-
-        depressionCamera.transform.position = camera.position;
-        depressionCamera.transform.rotation = Quaternion.Euler(new Vector3(camRotation, playerRotation, 0f));
-        InputManager.instance.RemRegControl(false);
         CinematicManager.instance.CameraChange(depressionCamera);
-
-        yield return new WaitForEndOfFrame();
-
-        while (CinematicManager.instance.cinemachineBrain.IsBlending)
-            yield return new WaitForEndOfFrame();
-
-
-        Rigidbody rb = player.GetComponent<Rigidbody>();
-
-        rb.interpolation = RigidbodyInterpolation.None;
-        player.rotation = Quaternion.Euler(new Vector3(0f, playerRotation, 0f));
-
-        camera.localRotation = Quaternion.Euler(new Vector3(camRotation, 0f, 0f));
-        camera.GetComponent<CameraLook>().ChangeRotation(camRotation);
-        CinematicManager.instance.ReturnPlayerCamera();
-        InputManager.instance.RemRegControl(true);
-
-        yield return new WaitForEndOfFrame();
-        yield return new WaitForEndOfFrame();
-
-
-        rb.interpolation = RigidbodyInterpolation.Interpolate;
-
+        InputManager.instance.RemRegControl(false);
+        Invoke(nameof(LockToDepression), 0.1f);
     }
-    public override void StartMinigame()
+
+    void LockToDepression()
     {
-        Cursor.lockState = CursorLockMode.Confined;
-        //arrancar el tema
-        foreach (KeypadButton keypadButton in buttons)
-            keypadButton.gameObject.SetActive(true);
-        StartCoroutine("BeatTimer");
-    }
-
-    public void RestartMinigame()
-    {
-        StopCoroutine("BeatTimer");
-        //fade out de la música
-
-        //devolver la camara al jugador
-        CinematicManager.instance.ReturnPlayerCamera();
+        foreach (var renderer in renderers)
+            renderer.materials[0].SetTexture("_EmissionMap", keypadTextures[interactedTimes]);
+        cameraLook.ChangeRotation(depressionCamera.transform.rotation);
         InputManager.instance.RemRegControl(true);
-        Cursor.lockState = CursorLockMode.Locked;
-
-        //los botones vuelven a ponerse en verde
-        buttons = GetComponentsInChildren<KeypadButton>().ToList();
-        foreach (Material material in GetComponent<Renderer>().materials)
-            if (material.name == "Button (Instance)")
-                material.SetColor("_Color", Color.green);
-    }
-    IEnumerator BeatTimer()
-    {
-        int currBeat = 0;
-        while(currBeat < beats.Length)
-        {
-            float deltaBeatTime = currBeat > 0 ? beats[currBeat].beatTime - beats[currBeat - 1].beatTime : beats[currBeat].beatTime;
-            yield return new WaitForSeconds(deltaBeatTime);
-
-            List<KeypadButton> availableButtons = buttons.Where(x => !x.waitingForBeat).ToList();
-            if(availableButtons.Count > 0)
-                StartCoroutine(availableButtons[Random.Range(0, availableButtons.Count)].WaitForBeat(beats[currBeat].beatDuration));
-            currBeat++;
-        }
-        bool waitingForEnd = true;
-        while(waitingForEnd)
-        {
-            yield return new WaitForEndOfFrame();
-
-            if(!Camera.main.GetComponent<AudioSource>().isPlaying)
-                waitingForEnd = false;
-        }
-
         CinematicManager.instance.ReturnPlayerCamera();
-        InputManager.instance.RemRegControl(true);
-        Cursor.lockState = CursorLockMode.Locked;
-        GetComponent<ElevatorArrival>().StartArrival();
-        Destroy(depressionPositions[depressionPositions.Length - 1]);
     }
-}
-
-[System.Serializable]
-class Beat
-{
-    public float beatTime;
-    public float beatDuration;
 }
